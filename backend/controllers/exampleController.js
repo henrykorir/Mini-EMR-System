bbc52dd7e94f4cfb627536ac3dda768eb9564b69
Henry Korir <longkhery@gmail.com>
2025-11-14 16:28:17 +0000

    chore(skeleton): Set up the boilerplate skeleton code

--- backend/controllers/exampleController.js
+++ backend/controllers/exampleController.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql');
+require('dotenv').config();
+
+// Access environment variables
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbDatabase = process.env.DB_DATABASE;
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Admin123',
-  database: 'medigrind'
+  host: dbHost,
+  user: dbUser,
+  password: dbPassword,
+  database: dbDatabase
 });
 
 exports.getSum = (req, res) => {
